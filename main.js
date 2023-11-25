f3f6d37ce610203584dbf8adef3866df0bdf9808
tanyshkakoval28@gmail.com <tanyshkakoval28@gmail.com>
2023-11-25 21:48:14 +0200

    home work

---
+++ main.js
@@ -0,0 +1,22 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+        return this
+    }
+};
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+
+ladder.up().up().down().showStep();
